09e70856e0d871fb00d928b7d2b6760eedb60aab
Daniele Martignani <daniele.martignani@studio.unibo.it>
2021-12-22 18:15:09 +0100

    fixed prodotti consigliati

--- template/homeUser.php
+++ template/homeUser.php
@@ -167,7 +167,7 @@
 <div class="row">
     <h1>prodotti consigliati</h1>
     <div class="col-12">
-        <div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div id="carouselProdottiConsigliati" class="carousel carousel-dark slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <div class="col-sm-4">
@@ -204,11 +204,11 @@
                 </div>
             </div>
 
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselProdottiConsigliati" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselProdottiConsigliati" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
