67d707a4c45f51e595563f9ac087f9a1f00685a5
Daniele Martignani <daniele.martignani@studio.unibo.it>
2021-12-22 18:47:14 +0100

    deleted old code comments

--- template/homeUser.php
+++ template/homeUser.php
@@ -115,55 +115,6 @@
 </div>
 
 <!-- prodotti consigliati -->
-<!-- <div class="row">
-    <h1>prodotti consigliati</h1>
-    <div class="col-12">
-        <div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <div class="card" style="width: 18rem;">
-                                <img class="img-fluid" src="<?php echo UPLOAD_DIR . "Superficie.png"; ?>" alt="" />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-4">
-                            <div class="card" style="width: 18rem;">
-                                <img class="img-fluid" src="<?php echo UPLOAD_DIR . "Superficie.png"; ?>" alt="" />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-4">
-                            <div class="card" style="width: 18rem;">
-                                <img class="img-fluid" src="<?php echo UPLOAD_DIR . "Superficie.png"; ?>" alt="" />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </div>
-    </div>
-</div> -->
-
 <div class="row">
     <h1>prodotti consigliati</h1>
     <div class="col-12">
