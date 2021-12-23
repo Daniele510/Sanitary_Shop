2d7ff0b02fdbc586565509e7f0b4a0ac8bd5682e
Luca Grandi <luca.grandi9@studio.unibo.it>
2021-12-23 18:14:32 +0100

    agiunta la mod disabilitato al pulsante settings

--- template/lista-prodotti.php
+++ template/lista-prodotti.php
@@ -1,11 +1,11 @@
 <div class="row">
     <!-- filtri ricerca -->
     <div class="d-flex justify-content-end">
-        <button class="btn btn-settings" type="button" id="dropdownMenuClickable" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
-            <img class="img-fluid" src="<?php echo UPLOAD_DIR . "settings.svg"; ?>" alt="impostazioni ricerca" width="20" height="20" />
-        </button>
-        <?php if (isset($templateParams["prodotti"])) : ?>
-            <ul class="dropdown-menu col-10 col-md-6" aria-labelledby="dropdownMenuClickable" style="overflow-y:auto; max-height:50vh">
+        <?php if (count($templateParams["prodotti"]) > 0) : ?>
+            <button class="btn btn-settings" type="button" id="dropdownMenuClickable" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
+                <img class="img-fluid" src="<?php echo UPLOAD_DIR . "settings.svg"; ?>" alt="impostazioni ricerca" width="20" height="20" />
+            </button>
+            <ul class="dropdown-menu col-10 col-md-6" aria-labelledby="dropdownMenuClickable" style="overflow-y:auto; max-height:50vh;">
                 <li>
                     <h3>filtri</h3>
                     <ul style="list-style: none;">
@@ -54,12 +54,13 @@
                     </ul>
                 </li>
                 <li>
-                    <div class="d-flex justify-content-end">
-                        <button type="button" class="btn" style="margin-right: 20px; outline: 1px solid red;" disabled>Salva modifiche</button>
-                        <button type="button" class="btn" style="outline: 1px solid red;">Annulla</button>
-                    </div>
+                    <button type="button" class="btn" style="margin-right: 20px; outline: 1px solid red;" disabled>Salva modifiche</button>
                 </li>
             </ul>
+        <?php else : ?>
+            <button class="btn btn-settings" type="button" disabled>
+                <img class="img-fluid" src="<?php echo UPLOAD_DIR . "settings.svg"; ?>" alt="impostazioni ricerca" width="20" height="20" />
+            </button>
         <?php endif; ?>
     </div>
     <?php if (count($templateParams["prodotti"]) > 0) : ?>
@@ -68,13 +69,12 @@
                 <div class="row g-0 align-items-center">
                     <div class="col-4">
                         <!--TODO: inserire dati presi da database -->
-                        <img src="<?php echo $prodotto["img"]; ?>" class="img-fluid rounded-start" alt="" />
+                        <img src="<?php echo $prodotto["Img"]; ?>" class="img-fluid rounded-start" alt="" />
                     </div>
                     <div class="col-8">
                         <div class="card-body overflow-hidden">
                             <!--  -->
                             <h5 class="card-title"><?php echo $prodotto["NomeProdotto"]; ?></h5>
-                            <p class="card-text"><?php echo $prodotto["BreveDescrizione"]; ?></p>
                             <div class="row d-flex align-items-center">
                                 <p class="card-text col-6 m-0"><?php echo $prodotto["prezzo"]; ?></p>
                                 <a class="card-text col-6 d-flex justify-content-end" href="#"><img class="img-fluid" src="<?php echo UPLOAD_DIR . "carbon_shopping-cart-plus.svg"; ?>" alt="aggungi al carrello" /></a>
