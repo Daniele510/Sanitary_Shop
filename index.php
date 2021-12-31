2ed7c54e4275f8f14e2fc925273cc640a75312b4
Grandi-Luca <73893837+Grandi-Luca@users.noreply.github.com>
2021-12-31 20:17:21 +0100

    Presi n prodotti scontati dal database

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once 'connection.php';
 $templateParams["titolo"] = "titoloUser.php";
 $templateParams["home"] = "homeUser.php";
 $templateParams["categorie"] = $dbh->getCategories();
+$templateParams["prodotti_scontati"] = $dbh->getRandomDiscountedProduct(3);
 
 require './template/baseUser.php';
 
