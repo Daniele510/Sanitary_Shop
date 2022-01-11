8d71672be89d307c6760c4a1fe3745c7195120ae
Luca Grandi <luca.grandi9@studio.unibo.it>
2022-01-11 16:16:22 +0100

    creata page login aziende

---
+++ template-aziende/login-form.php
@@ -0,0 +1,38 @@
+<div class="row d-flex justify-content-center">
+    <div class="row d-flex justify-content-center">
+        <h1 class="col-6" style="margin: 40px 0 0 0; text-align: center;">WELCOME!</h1>
+    </div>
+    <form action="#" method="POST" class="col-10 col-md-8 needs-validation d-flex flex-column" style="margin-top: 1.875rem; gap: 2rem; padding-bottom: 2rem; padding-top: 1rem; background: white; border-radius: 10px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);" novalidate>
+        <?php if (isset($templateParams["errorelogin"])) : ?>
+            <div class="row p-0 text-center">
+                <p class="m-0" style="color: red;"><?php echo $templateParams["errorelogin"]; ?></p>
+            </div>
+        <?php endif; ?>
+        <div class="row">
+            <label for="validationEmail" class="col-sm-2 col-form-label form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" id="validationEmail" name="Email" required>
+                <div class="invalid-feedback">
+                    Please insert a valid email address
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <label for="validationPassword" class="col-sm-2 col-form-label form-label">Password</label>
+            <div class="col-sm-10">
+                <input class="form-control" type="password" id="validationPassword" name="Password" required>
+                <div class="invalid-feedback">
+                    Please fill the password's field
+                </div>
+            </div>
+        </div>
+        <div class="row p-0 d-flex justify-content-center">
+            <button class="col-5 col-sm-3 btn" type="submit" style="background-color: #06ACB8; border-radius: 10px; color: white;">Login</button>
+        </div>
+        <div class="row d-flex justify-content-center align-items-center" style="padding: 0 12.750px;">
+            <div class="col-4 p-0" style="text-align: center;">
+                <a href="user-registration.php" style="text-decoration: none; font-style: italic; font-weight: lighter; font-size: 16px;">Registrati ora</a>
+            </div>
+        </div>
+    </form>
+</div>
