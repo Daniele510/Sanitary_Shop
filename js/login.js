39f4575974150a3713ff34999a57b12cddce2c00
Luca Grandi <luca.grandi9@studio.unibo.it>
2022-01-14 10:34:27 +0100

    cambiato il comportamento della schermata di login on resize

--- js/login.js
+++ js/login.js
@@ -17,4 +17,22 @@ $(document).ready(function() {
             }, false)
         });
     
+    checkWidth();
+
+    $( window ).resize(function() {
+        checkWidth();
+    });
+    
+    function checkWidth(){    
+        //Check condition for screen width
+        if($(window).width() > 768){
+            if(!$('.grid-container > #notifiche').hasClass("container")){
+                $('.grid-container > #notifiche').addClass("container");
+            }
+        } else{
+            if($('.grid-container > #notifiche').hasClass("container")){
+                $('.grid-container > #notifiche').removeClass("container");
+            }
+        }
+    }
 });
--- template/login-home.php
+++ template/login-home.php
@@ -1,18 +1,33 @@
 <div class="row d-flex justify-content-center p-0 m-0">
-    <div class="col-11 d-flex flex-column align-items-center" style="row-gap: 2.375rem; margin-top: 2.375rem;">
-        <div class="col-12 d-flex justify-content-between p-0">
-            <div class="col-10">
-                <button class="col-5 col-sm-3 btn outline_primary">ORDINI</button>
-            </div>
-            <div class="col-1">
-                <a href="#"><img src="<?php echo UPLOAD_DIR . "iconImgs/bell.svg"; ?>" alt="notifiche"></a>
+    <div class="col-11 grid-container" style="margin-top: 2.375rem;">
+        <div class="d-flex justify-content-start">
+            <button class="col-6 btn outline_primary">ORDINI</button>
+        </div>
+        <div id="notifiche">
+            <a href="#"><img src="<?php echo UPLOAD_DIR . "iconImgs/bell.svg"; ?>" alt="notifiche"></a>
+            <div class="col">
+                <?php for ($i = 0; $i < 5; $i++) : ?>
+                    <div class="card col-12">
+                        <div class="row d-flex align-items-stretch">
+                            <div class="col-5">
+                                <img src="upload/categoryImgs/Bagno.png" class="img-fluid" alt="" />
+                            </div>
+                            <div class="col-7">
+                                <div class="card-body d-flex flex-column p-0">
+                                    <h5 class="card-title m-0">Lorem ipsum dolor, sit amet</h5>
+                                    <p class="card-text m-0">pippo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endfor; ?>
             </div>
         </div>
-        <div class="col-12 d-flex">
-            <h1 class="m-0 p-0">Informazioni personali</h1>
+        <div class="d-flex">
+            <h2 class="m-0 p-0">Informazioni personali</h2>
         </div>
-        <div class="col-12 d-flex flex-column p-0 m-0 info_container" style="gap: 2rem;">
-            <div class="col col-sm-5 d-flex flex-column" style="row-gap: 1.25rem; background-color: white; border-radius: 10px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); padding: 10px;">
+        <div class="row container-dati">
+            <div class="col-12 container">
                 <h3 class="mb-0">Dati spedizione</h3>
                 <p class="mb-0">
                     <?php echo $templateParams["info-utente"][0]["NomeCompleto"]; ?><br />
@@ -25,7 +40,9 @@
                 </p>
                 <a class="col-7 col-sm-5 btn primary d-flex justify-content-center" href="login.php?action=mod-info-spedizione">Modifica</a>
             </div>
-            <div class="col col-sm-5 d-flex flex-column" style="row-gap: 1.25rem; background-color: white; border-radius: 10px; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); padding: 10px;">
+        </div>
+        <div class="row">
+            <div class="col-12 container">
                 <h3 class="mb-0">Modalità di pagamento</h3>
                 <p class="mb-0">
                     ****<?php echo $templateParams["info-utente"][0]["CodCarta"]; ?><br />
