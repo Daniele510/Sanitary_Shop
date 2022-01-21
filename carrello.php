bb35f81ac686ed18b333dafd54f50c21098aaf64
Daniele Martignani <daniele.martignani@studio.unibo.it>
2022-01-21 21:20:09 +0100

    Updated carrello.php and userCart.php

--- carrello.php
+++ carrello.php
@@ -3,12 +3,44 @@
 require_once 'connection.php';
 
 $templateParams["home"] = "userCart.php";
-$templateParams["titolo"] = "titoloUser.php";
+$templateParams["titolo"] = "header.php";
 $templateParams["titolo_pagina"] = "Il tuo carrello è vuoto";
+$numArticoli = 0;
 
-$totaleCassa="";
-$articoli=array();
-$numArticoli= count($articoli);
+if(isset($POST["aggiungi_al_carrello"])){ 
+    if(isset($COOKIE["carrello"])){
+        $cookie_data = stripslashes($_COOKIE['carrello']);
+        $templateParams["dati_carrello"] = json_decode($cookie_data, true);
+    }else{
+        $templateParams["dati_carrello"]=array();
+    }
+    $lista_cod_prodotti = array_column($templateParams["dati_carrello"], 'id_prodotto');
+    
+    if(in_array($_POST["id_nascosto"],$lista_cod_prodotti)){
+        foreach($templateParams["dati_carrello"] as $keys => $values){
+            if($templateParams["dati_carrello"][$keys]['id_prodotto'] == $_POST["id_nascosto"]){
+                $templateParams["dati_carrello"][$keys]["quantità_prodotto"] = $templateParams["dati_carrello"][$keys]["quantità_prodotto"] + $_POST["quantità"];
+            }
+        }
+    }else{
+        $array_dati = array(
+            'id_prodotto' => $_POST["id_nascosto"],
+            'nome_prodotto' => $_POST["nome_nascosto"],
+            'prezzo_prodotto' => $_POST["prezzo_nascosto"],
+            'quantità_prodotto' => $_POST["quantità"]
+        );
+        $templateParams["dati_carrello"][] = $array_dati;
+    }
+
+    $dati_prodotto = json_encode($templateParams["dati_carrello"]);
+    setcookie('carrello', $dati_prodotto, time()+(86400*30));
+    header("location:userCart.php?success=1");
+
+}
+
+
+$templateParams["totaleCassa"]="";
+
 $testo1= "articolo";
 $testo2= "articoli";
-require 'template/baseUser.php';
+require 'template/base.php';
--- template/userCart.php
+++ template/userCart.php
@@ -1,10 +1,9 @@
 <div class="row">
-    <h1><?php echo $templateParams["titolo_pagina"] ?></h1>
       <div class="card col-10" style="display: flex; border-radius: 5px; margin: 20px 30px 0px" id="">
         <div class="row g-0" style="flex-direction: row-reverse; justify-content: space-evenly; align-items: stretch; margin-bottom: 1rem;">
           <div class="col4">
             <div class="card-body" style="padding-right: 0; padding-bottom: 0;">
-              <h1 class="card-title">Totale</h1>
+              <h1 class="card-title">Totale: <!--<?php echo number_format($total, 2); ?> --></h1> 
               <a class="btn btn-dark" href="#" style="align-self: flex-end; padding: 1.1rem 3.8rem; border-radius: 10px; background: #324B4B;">Vai alla cassa(<?php echo $numArticoli; ?> <?php if($numArticoli==0 or $numArticoli>1) echo $testo2; else echo $testo1; ?>) </a>
             </div>
           </div>
@@ -12,29 +11,45 @@
       </div>
     </div>
     <div class="vstack col-md-5 mx-auto" style="gap: 2rem;">
-        <?php if (count($templateParams["prodotti"]) > 0 && $templateParams["titolo_pagina"] !== 'Categoria assente') : ?>
-            <?php foreach ($templateParams["prodotti"] as $prodoto) : ?>
-                <div style="display: flex;">
-                    <div class="col-1"></div>
-                    <div class="card col-10" style="border-radius: 10px;">
-                        <div class="row g-0 d-flex align-items-stretch">
-                            <div class="col-5">
-                                <img src="<?php echo UPLOAD_DIR . $prodoto["ImgPath"]; ?>" class="img-fluid rounded-start" alt="" style="min-height: 152px;"/>
-                            </div>
-                            <div class="col-7">
-                                <div class="card-body overflow-hidden d-flex flex-column" style="height: 100%;">
-                                    <h5 class="card-title m-0"><?php echo $prodoto["NomeProdotto"]; ?></h5>
-                                    <div class="my-auto row d-flex align-items-center justify-content-between">
-                                        <p class="card-text col-8 m-0"><?php echo $prodoto["Prezzo"]; ?></p>
-                                        <span class="col-2 d-flex justify-content-end"><img src="<?php echo UPLOAD_DIR . "carbon_shopping-cart-plus.svg"; ?>" alt="aggungi al carrello" style="height: 20px;" /></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-1"></div>
+        <?php
+        if(isset($_COOKIE["shopping_cart"]))
+        {
+        $totale = 0;
+        $cookie_data = stripslashes($_COOKIE['shopping_cart']);
+        $templateParams["dati_carrello"] = json_decode($cookie_data, true);
+        foreach($cart_data as $keys => $values)
+        {
+        ?>
+        <div style="display: flex;">
+          <div class="col-1"></div>
+            <div class="card col-10" style="border-radius: 10px;">
+              <div class="row g-0 d-flex align-items-stretch">
+                <div class="col-5">
+                  <img src="<?php echo UPLOAD_DIR . $values["ImgPath"]; ?>" class="img-fluid rounded-start" alt="" style="min-height: 152px;"/>
                 </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
+                <div class="col-7">
+                  <div class="card-body overflow-hidden d-flex flex-column" style="height: 100%;">
+                    <h5 class="card-title m-0"><?php echo $values["nome_prodotto"]; ?></h5>
+                      <div class="my-auto row d-flex align-items-center justify-content-between">
+                        <p class="card-text col-8 m-0"><?php echo $values["prezzo_prodotto"]; ?></p>
+                        <p class="card-text col-8 m-0"><?php echo $values["quantità_prodotto"]; ?></p>
+                        <span class="col-2 d-flex justify-content-end"><img src="<?php echo UPLOAD_DIR . "carbon_shopping-cart-plus.svg"; ?>" alt="aggungi al carrello" style="height: 20px;" /></span>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <div class="col-1"></div>
+        </div>
+        <?php $templateParams["totaleCassa"] =  $templateParams["totaleCassa"] + ($values["quantità_prodotto"] * $values["prezzo_prodotto"]);
+        }
+        }
+        else
+        {
+        ?>
+          <h1 style="margin: 40px 0 0 0; text-align: center;"><?php echo $templateParams["titolo_pagina"]?></h1>
+        <?php
+        }
+        ?> 
     </div>
 </div>
