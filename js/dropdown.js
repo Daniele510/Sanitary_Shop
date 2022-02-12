aaefa27bf00c52ecfe6dc6aaec116889b4ee1bf3
Luca Grandi <luca.grandi9@studio.unibo.it>
2022-02-12 13:25:13 +0100

    aggiunto lo stile alla pagina dei risulati della ricerca di un prodotto

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
     $(".btn-settings").click(function(){
         if($(".btn-settings > img").attr("src")==="./upload/x-icon.svg"){
             $(".btn-settings > img").attr("src","./upload/settings.svg");
+            $(".btn-settings > img").attr("alt","filtri ricerca");
         } else{
             $(".btn-settings > img").attr("src","./upload/x-icon.svg");
+            $(".btn-settings > img").attr("alt", "chiudi filtri ricerca");
         }
+        $(".transform").toggleClass("transform-active");
+        $("#background").toggleClass("background-active");
     });
 });
--- template/ris-ricerca.php
+++ template/ris-ricerca.php
@@ -1,89 +1,82 @@
-<div class="row">
+<div class="row p-0 m-0">
     <!-- filtri ricerca -->
-    <!-- FIXME: trasformare in modal -->
-    <div class="row p-0 m-0 justify-content-center">
-        <div class="col-10 p-0 d-flex justify-content-end">
-            <?php if (count($templateParams["prodotti"]) > 0) : ?>
-                <button class="btn btn-settings mt-3" type="button" id="dropdownMenuClickable" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
-                    <img src="<?php echo UPLOAD_DIR . "settings.svg"; ?>" alt="impostazioni ricerca" width="20" height="20" />
-                </button>
-                <ul class="dropdown-menu col-10 col-md-6" aria-labelledby="dropdownMenuClickable" style="overflow-y:auto; max-height:50vh;">
-                    <li>
-                        <h3>filtri</h3>
-                        <ul style="list-style: none;">
-                            <?php foreach ($templateParams["prodotti"] as $prodotto) : ?>
+    <?php if (isset($templateParams["prodotti"]) && count($templateParams["prodotti"]) > 0) : ?>
+        <div class="row p-0 m-0 justify-content-center">
+            <div class="col-10 mt-3 p-0 d-flex justify-content-end">
+                <div class="filter-container transform">
+                    <button class="btn btn-settings">
+                        <img src="<?php echo UPLOAD_DIR . "settings.svg"; ?>" alt="impostazioni ricerca"/>
+                    </button>
+                    <ul>
+                        <li>
+                            <h3>Filtra per</h3>
+                            <ul>
+                                <?php foreach ($templateParams["prodotti"] as $prodotto) : ?>
+                                    <li>
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                        <label class="form-check-label" for="flexCheckDefault">
+                                            <?php echo $prodotto["NomeCompagnia"]; ?>
+                                        </label>
+                                    </li>
+                                <?php endforeach ?>
+                            </ul>
+                        </li>
+                        <li>
+                            <h3>Ordina per</h3>
+                            <ul>
                                 <li>
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        <?php echo $prodotto["NomeCompagnia"]; ?>
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="check_price_up">
+                                    <label class="form-check-label" for="check_price_up">
+                                        prezzo crescente
                                     </label>
                                 </li>
-                            <?php endforeach ?>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-
-                    <li>
-                        <h3>ordina per</h3>
-                        <ul style="list-style: none;">
-                            <li>
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                                <label class="form-check-label" for="flexRadioDefault1">
-                                    prezzo crescente
-                                </label>
-                            </li>
-                            <li>
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                                <label class="form-check-label" for="flexRadioDefault1">
-                                    prezzo decrescente
-                                </label>
-                            </li>
-                            <li>
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                                <label class="form-check-label" for="flexRadioDefault1">
-                                    sconto decrescente
-                                </label>
-                            </li>
-                            <li>
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                                <label class="form-check-label" for="flexRadioDefault1">
-                                    nome prodotto
-                                </label>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <button type="button" class="btn btn-outline-danger"  disabled>Salva modifiche</button>
-                    </li>
-                </ul>
-            <?php else : ?>
-                <button class="btn btn-settings mt-3" type="button" disabled>
-                    <img class="img-fluid" src="<?php echo UPLOAD_DIR . "settings.svg"; ?>" alt="impostazioni ricerca" width="20" height="20" />
-                </button>
-            <?php endif; ?>
+                                <li>
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="check_price_down">
+                                    <label class="form-check-label" for="check_price_down">
+                                        prezzo decrescente
+                                    </label>
+                                </li>
+                                <li>
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="check_discount">
+                                    <label class="form-check-label" for="check_discount">
+                                        sconto decrescente
+                                    </label>
+                                </li>
+                                <li>
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="check_name">
+                                    <label class="form-check-label" for="check_name">
+                                        nome prodotto
+                                    </label>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <button type="button" class="btn btn-outline-danger">Salva modifiche</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="row container-list mt-3">
-        <!-- elenco risultati se presenti -->
-        <?php if (isset($templateParams["prodotti"]) && count($templateParams["prodotti"]) > 0) :
-            foreach ($templateParams["prodotti"] as $prodotto) : ?>
-                <div class="card col-10">
-                    <div class="row g-0 p-0 m-0 align-items-center">
-                        <div class="col-4">
-                            <img src="<?php echo UPLOAD_DIR . $prodotto["ImgPath"]; ?>" class="img-fluid" alt="" />
-                        </div>
-                        <div class="col-8 p-0 m-0">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $prodotto["NomeProdotto"]; ?></h5>
-                                <p class="card-text m-0"><?php echo round($prodotto["Prezzo"], 2); ?></p>
+
+        <div class="row container-list mt-3">
+            <!-- elenco risultati se presenti -->
+                <?php foreach ($templateParams["prodotti"] as $prodotto) : ?>
+                    <div class="card col-10">
+                        <div class="row g-0 p-0 m-0 align-items-center">
+                            <div class="col-4">
+                                <img src="<?php echo UPLOAD_DIR . $prodotto["ImgPath"]; ?>" class="img-fluid" alt="" />
+                            </div>
+                            <div class="col-8 p-0 m-0">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $prodotto["NomeProdotto"]; ?></h5>
+                                    <p class="card-text m-0"><?php echo round($prodotto["Prezzo"], 2); ?></p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-        <?php endforeach;
-        endif; ?>
-    </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+    <!-- TODO: inserire immagine errore nel caso non ci siano prodotti -->
 </div>
+<div aria-hidden="true" id="background"></div>
