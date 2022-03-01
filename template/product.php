eb66ed17d5747330a7357dcaada83c59ca652749
Luca <luca.grandi9@studio.unibo.it>
2022-03-01 17:27:54 +0100

    aggiustato bug grafico schermata prodotto

--- template/product.php
+++ template/product.php
@@ -1,19 +1,19 @@
 <div class="row">
-    <?php if $templateParams["titolo_pagina"] !== 'Prodotto non esistente') : ?>
+    <?php if ($templateParams["titolo_pagina"] !== 'Prodotto non esistente') : ?>
         <div class="product content-wrapper">
-            <img src="<?php echo UPLOAD_DIR . $templateParams["prodotto"]["ImgPath"];?>" width="500" height="500" alt="<?=$templateParams["prodotto"]['nomeProdotto']?>">
+            <img src="<?php echo UPLOAD_DIR . $templateParams["prodotto"]["ImgPath"];?>" width="500" height="500" alt="<?php echo $templateParams["prodotto"]['NomeProdotto']?>">
             <div>
-                <h1 class="name"><?=$templateParams["prodotto"]['nomeProdotto']?></h1>
+                <h1 class="name"><?php echo $templateParams["prodotto"]['NomeProdotto']?></h1>
                 <span class="price">
-                    &euro;<?=$templateParams["prodotto"]['PrezzoUnitario']?>
+                    &euro;<?php echo $templateParams["prodotto"]['Prezzo']?>
                 </span>
                 <form method="post">
-                    <input type="number" name="quantity" value="1" min="1" max="<?=$templateParams["prodotto"]['MaxQtaMagazzino']?>" placeholder="Quantity" required>
-                    <input type="hidden" name="product_id" value="<?=$templateParams["prodotto"]['CodProdotto']?>">
+                    <input type="number" name="quantity" value="1" min="1" max="<?php echo $templateParams["prodotto"]['MaxQtaMagazzino']?>" placeholder="Quantity" required>
+                    <input type="hidden" name="product_id" value="<?php echo $templateParams["prodotto"]['CodProdotto']?>">
                     <input type="submit" value="Add To Cart">
                 </form>
                 <div class="description">
-                    <?=$templateParams["prodotto"]['Descrizione']?>
+                    <?php echo $templateParams["prodotto"]['Descrizione']?>
                 </div>
             </div>
         </div>
