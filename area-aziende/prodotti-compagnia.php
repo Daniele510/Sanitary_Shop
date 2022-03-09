c815e09b2b6947191cb749aea370da7adce21746
Luca <luca.grandi9@studio.unibo.it>
2022-03-09 13:13:19 +0100

    prova inserimento immagini nella cartella relativa

--- area-aziende/prodotti-compagnia.php
+++ area-aziende/prodotti-compagnia.php
@@ -16,6 +16,7 @@ if(isset($_GET["action"]) && $_GET["action"]==="ins-new-prod"){
     $templateParams["home"] = "nuovo-prodotto-form.php";
     $templateParams["header"] = "header.php";
     $templateParams["js"] = [];
+    $templateParams["categorie"] = $dbh->getCategories();
     require '../template-azienda/base.php';
     return;
 }
